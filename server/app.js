194dacd2de498ef7585c0d093d9120374a9d6f4e
JakSzaq <szafrykj@gmail.com>
2023-12-16 23:33:16 +0100

    added new cors origin for future deployment

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://traverse-jn61.onrender.com'],
     credentials: true,
     optionsSuccessStatus: 200,
   })
