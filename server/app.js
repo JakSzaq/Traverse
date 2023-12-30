ea4894bcc8c22399db70db0503332cc423a91e90
JakSzaq <szafrykj@gmail.com>
2023-12-30 15:46:59 +0100

    Solved the issue with path on server deployment

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 const schedule = require('node-schedule');
 
 const AppError = require('./utils/appError');
